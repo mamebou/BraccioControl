03f50c57a849a1004b255d683e72a5bbcb852c01
mamebou <mamebou0810@outlook.jp>
2024-08-02 19:23:59 +0900

    fix

---
+++ BraccioIKUnity/Assets/Scripts/ExperimentManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentManager : MonoBehaviour
+{
+    public float[] accuracyResult = new float[3]; 
+    public int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ BraccioIKUnity/Assets/Scripts/targetControllerLatest.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetControllerLatest : MonoBehaviour
+{
+    public Vector3 positionDif;
+    public GameObject manager;
+    private ExperimentManager exManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exManager = manager.GetComponent<ExperimentManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "block"){
+            Debug.Log("hgvjfkhgskjhfglsdhfd");
+            exManager.accuracyResult[exManager.index] = Vector3.Distance(this.transform.position, collision.gameObject.transform.position);
+            exManager.index += 1;
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
